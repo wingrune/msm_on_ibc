d0554583effd8080aadf33f84d7306ceb5648305
alexis <alexisthual@gmail.com>
2021-11-29 17:36:13 +0100

    Make package; debug example on drago

--- msm/lambda_steadiness.py
+++ msm/lambda_steadiness.py
@@ -6,20 +6,20 @@ import random
 from scipy import stats
 
 import nibabel as nib
-import matplotlib.pyplot as plt  # noqa: E402
-from run_msm import run_msm, prepare_darrays
+import matplotlib.pyplot as plt
+from run import run_msm, prepare_darrays
 
 dotenv.load_dotenv()
 FSL_PATH = os.getenv("FSL_PATH")
 FSL_CONFIG_PATH = os.getenv("FSL_CONFIG_PATH")
 
 # Load spherical mesh produced with `mris_convert`
-spherical_mesh = './data/lh.sphere.gii'
+spherical_mesh = "./data/lh.sphere.gii"
 
 # load data
-subject_input = 'sub-07'
-subject_reference = 'sub-04'
-data_path = './data/'
+subject_input = "sub-07"
+subject_reference = "sub-04"
+data_path = "./data/"
 
 # parameters
 random.seed(42)
@@ -28,10 +28,10 @@ lambd = [0.075, 0.1, 0.15]
 train_mode = True
 
 sessions = [
-    ['ses-00'],
-    ['ses-00', 'ses-01', 'ses-02', 'ses-04'],
-    ['ses-00', 'ses-01'],
-    ['ses-00', 'ses-01', 'ses-02'],
+    ["ses-00"],
+    ["ses-00", "ses-01", "ses-02", "ses-04"],
+    ["ses-00", "ses-01"],
+    ["ses-00", "ses-01", "ses-02"],
 ]
 
 for session in sessions:
@@ -42,17 +42,17 @@ for session in sessions:
     data_input = [
         os.path.join(data_path, file)
         for file in os.listdir(data_path)
-        if subject_input in file and 'lh' in file and (
-            any(ses in file for ses in session)
-        )
+        if subject_input in file
+        and "lh" in file
+        and (any(ses in file for ses in session))
     ]
 
     data_reference = [
         os.path.join(data_path, file)
         for file in os.listdir(data_path)
-        if subject_reference in file and 'lh' in file and (
-            any(ses in file for ses in session)
-        )
+        if subject_reference in file
+        and "lh" in file
+        and (any(ses in file for ses in session))
     ]
 
     for input_fname in data_input:
@@ -65,11 +65,10 @@ for session in sessions:
     print(len(data))
     # split data to train and test
 
-    train_data_input = random.sample(list(data.keys()), int(len(data)*0.8))
+    train_data_input = random.sample(list(data.keys()), int(len(data) * 0.8))
 
     train_data = {
-        input_fname: data[input_fname]
-        for input_fname in train_data_input
+        input_fname: data[input_fname] for input_fname in train_data_input
     }
 
     test_data = {
@@ -92,13 +91,16 @@ for session in sessions:
             f.writelines(list_of_lines)
 
         # running MSM with train data
-        output_dir = 'test_outputs_lambda_' + str(len(data)) + "_" + str(lam)
+        output_dir = "test_outputs_lambda_" + str(len(data)) + "_" + str(lam)
 
         if train_mode and not os.path.isdir(output_dir):
             transformed_mesh, transformed_func = run_msm(
-                in_data_list=list(train_data.keys()), in_mesh=spherical_mesh,
+                in_data_list=list(train_data.keys()),
+                in_mesh=spherical_mesh,
                 ref_data_list=list(train_data.values()),
-                debug=False, verbose=True, output_dir=output_dir
+                debug=False,
+                verbose=True,
+                output_dir=output_dir,
             )
 
         # testing MSM with test data
@@ -113,34 +115,34 @@ for session in sessions:
 
         for input_fname, reference_fname in test_data.items():
             data_input = nib.load(input_fname)
-            data_input.darrays = prepare_darrays(
-                data_input.darrays, coordsys
-            )
+            data_input.darrays = prepare_darrays(data_input.darrays, coordsys)
             # copy dataset in order to cope with a bug of MSM
-            extra_data = nib.load(input_fname) 
-            data_input.darrays.extend(prepare_darrays(
-                extra_data.darrays, coordsys
-            ))
-            filename = str(Path(output_dir) / 'input_test.func.gii')
+            extra_data = nib.load(input_fname)
+            data_input.darrays.extend(
+                prepare_darrays(extra_data.darrays, coordsys)
+            )
+            filename = str(Path(output_dir) / "input_test.func.gii")
             data_input.to_filename(filename)
             input_fname_preprocessed = filename
 
             input_contrast_name = input_fname.split(subject_input)[1]
             test_transformed = os.path.join(
                 output_dir,
-                f"test_transformed_and_projected{input_contrast_name}"
+                f"test_transformed_and_projected{input_contrast_name}",
             )
-            cmd = ' '.join([
-                f"{FSL_PATH}/fsl/bin/msmresample",
-                f"{deformed} ",
-                test_transformed,
-                f"-labels {input_fname_preprocessed} ",
-                f"-project {spherical_mesh}",
-            ])
+            cmd = " ".join(
+                [
+                    f"{FSL_PATH}/fsl/bin/msmresample",
+                    f"{deformed} ",
+                    test_transformed,
+                    f"-labels {input_fname_preprocessed} ",
+                    f"-project {spherical_mesh}",
+                ]
+            )
             exit_code = os.system(cmd)
             if exit_code != 0:
                 raise RuntimeError(f"Failed to run MSM with command:\n{cmd}")
-    
+
             # compute cross_correlation
             transformed_data_path = f"{test_transformed}.func.gii"
             transformed_data = nib.load(transformed_data_path).darrays[0].data
@@ -148,11 +150,11 @@ for session in sessions:
 
             cross_correlation[lam] += stats.pearsonr(
                 transformed_data, reference_data
-            )[0]/len(test_data)
+            )[0] / len(test_data)
             base_cross_correlation[lam] += stats.pearsonr(
                 data_input, reference_data
-            )[0]/len(test_data)
-        
+            )[0] / len(test_data)
+
     print(cross_correlation)
     print(base_cross_correlation)
     f1 = plt.figure()
@@ -160,12 +162,16 @@ for session in sessions:
     plt.ylabel("Pearson correlation")
     plt.title(f"Test data, data number: {len(data)}")
     plt.plot(
-        lambd, list(cross_correlation.values()),
-        '-o', label="Pearson correlation after transformation"
+        lambd,
+        list(cross_correlation.values()),
+        "-o",
+        label="Pearson correlation after transformation",
     )
     plt.plot(
-        lambd, list(base_cross_correlation.values()),
-        '-o', label="Pearson correlation before transformation"
+        lambd,
+        list(base_cross_correlation.values()),
+        "-o",
+        label="Pearson correlation before transformation",
     )
     plt.legend()
     plt.savefig(str(len(data)) + "lambda_optimization_test_data.png")
--- msm/plot.py
+++ msm/plot.py
@@ -1,23 +1,22 @@
-print("Importing nilearn...")
 from nilearn import plotting
 
 # Load spherical mesh produced with `mris_convert`
 print("Reading data...")
-spherical_mesh = './data/lh.sphere.gii'
+spherical_mesh = "./data/lh.sphere.gii"
 
 
 data_to_load = {
-    'sub-07': [
-        './data/sub-07_story-math_lh.gii',
-        './data/sub-07_relational_lh.gii',
+    "sub-07": [
+        "./data/sub-07_story-math_lh.gii",
+        "./data/sub-07_relational_lh.gii",
     ],
-    'sub-04': [
-        './data/sub-04_story-math_lh.gii',
-        './data/sub-04_relational_lh.gii',
-    ]
+    "sub-04": [
+        "./data/sub-04_story-math_lh.gii",
+        "./data/sub-04_relational_lh.gii",
+    ],
 }
 
-transformed_mesh = '/mnt/e/Ecole Polytechnique/Parietal/code/msm_on_ibc/test_outputs/transformed_in_mesh.surf.gii'
+transformed_mesh = "/mnt/e/Ecole Polytechnique/Parietal/code/msm_on_ibc/test_outputs/transformed_in_mesh.surf.gii"
 transformed_func = "/mnt/e/Ecole Polytechnique/Parietal/code/msm_on_ibc/test_outputs/transformed_and_reprojected.func.gii"
 
 
@@ -28,26 +27,27 @@ import matplotlib.pyplot as plt  # noqa: E402
 print("Plotting transformed data...")
 plotting.plot_surf(
     spherical_mesh,
-    transformed_func, title='Transformed Data - relational',
-    cmap="RdBu"
+    transformed_func,
+    title="Transformed Data - relational",
+    cmap="RdBu",
 )
-plt.savefig('./transformed_relational_7_to_4.png')
+plt.savefig("./transformed_relational_7_to_4.png")
 
 
 print("Plotting origin data...")
 plotting.plot_surf(
     spherical_mesh,
     "/mnt/e/Ecole Polytechnique/Parietal/code/msm_on_ibc/test_outputs/in_data_extra.func.gii",
-    title='Origin Data - relational',
-    cmap="RdBu"
+    title="Origin Data - relational",
+    cmap="RdBu",
 )
-plt.savefig('./origin_relational_7_to_4.png')
+plt.savefig("./origin_relational_7_to_4.png")
 
 print("Plotting reference data...")
 plotting.plot_surf(
     spherical_mesh,
     "/mnt/e/Ecole Polytechnique/Parietal/code/msm_on_ibc/test_outputs/ref_data_extra.func.gii",
-    title='Reference Data - relational',
-    cmap="RdBu"
+    title="Reference Data - relational",
+    cmap="RdBu",
 )
-plt.savefig('./reference_relational_7_to_4.png')
+plt.savefig("./reference_relational_7_to_4.png")
