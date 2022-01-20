f1967da241e7572b7313eae1298c704e9817dac0
alexis <alexisthual@gmail.com>
2022-01-20 14:56:28 +0100

    Add tests for model and run

---
+++ tests/test_model.py
@@ -0,0 +1,46 @@
+import pytest
+
+from msm import model
+from nilearn import datasets
+import numpy as np
+
+# from sklearn.utils.estimator_checks import check_estimator
+
+
+def test_create_model():
+    """Instantiate model without crash"""
+
+    try:
+        _ = model.MSM()
+    except Exception:
+        pytest.fail("Could not instantiate model")
+
+
+def test_align_random_data():
+    """Instantiate model without crash"""
+
+    m = model.MSM()
+
+    # generate random maps
+    fs3 = datasets.fetch_surf_fsaverage(mesh="fsaverage3")
+    n_voxels = 642
+    source_train_data = np.random.rand(2, n_voxels)
+    target_train_data = np.random.rand(2, n_voxels)
+
+    # align maps
+    m.fit(source_train_data, target_train_data, mesh_file=fs3.sphere_left)
+
+    # align new data from source to target
+    source_test_data = np.random.rand(4, n_voxels)
+    predicted_data = m.transform(source_test_data)
+    assert source_test_data.shape == predicted_data.shape
+
+    # check score
+    s = m.score(source_test_data, predicted_data)
+    assert isinstance(s, int) or isinstance(s, float)
+
+
+# def test_model_is_sklearn_estimator():
+#     """Model should have sklearn compatible API"""
+#
+#     check_estimator(model.MSM())
---
+++ tests/test_run.py
@@ -0,0 +1,65 @@
+import nibabel as nib
+from nibabel.gifti.gifti import GiftiDataArray, GiftiImage
+from nilearn import datasets
+import numpy as np
+import os
+from tempfile import TemporaryDirectory
+
+from msm import utils
+from msm import run
+
+
+def test_run():
+    """Main function run_msm should run without error."""
+
+    with TemporaryDirectory() as tmp_dir:
+        # Load fsaverage mesh
+        fs5 = datasets.fetch_surf_fsaverage(mesh="fsaverage3")
+        mesh_path = fs5.sphere_left
+        tmp_mesh_path = os.path.join(tmp_dir, os.path.basename(mesh_path[:-3]))
+        # Ungzip mesh to new file
+        utils.ungzip(mesh_path, tmp_mesh_path)
+
+        n_voxels = 642
+        mesh_coordsys = nib.load(tmp_mesh_path).darrays[0].coordsys
+
+        # Generate random contrast maps
+        # for source and target individuals
+        # with fsaverage coordinate system
+        source_contrasts_list = [
+            os.path.join(tmp_dir, "source1.gii"),
+            os.path.join(tmp_dir, "source2.gii"),
+        ]
+        for source_contrast in source_contrasts_list:
+            img = GiftiImage()
+            data = GiftiDataArray(
+                np.random.rand(n_voxels), coordsys=mesh_coordsys
+            )
+            img.add_gifti_data_array(data)
+            img.add_gifti_data_array(data)
+            nib.save(img, source_contrast)
+
+        target_contrasts_list = [
+            os.path.join(tmp_dir, "target1.gii"),
+            os.path.join(tmp_dir, "target2.gii"),
+        ]
+        for target_contrast in target_contrasts_list:
+            img = GiftiImage()
+            data = GiftiDataArray(
+                np.random.rand(n_voxels), coordsys=mesh_coordsys
+            )
+            img.add_gifti_data_array(data)
+            img.add_gifti_data_array(data)
+            nib.save(img, target_contrast)
+
+        mesh_gii, transformed_gii = run.run_msm(
+            source_contrasts_list,
+            tmp_mesh_path,
+            target_contrasts_list,
+        )
+
+        print(mesh_gii)
+        print(transformed_gii)
+
+        assert mesh_gii.darrays[0].data.shape[0] == n_voxels
+        assert transformed_gii.darrays[0].data.shape[0] == n_voxels
